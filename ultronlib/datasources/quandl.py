9c088ed88e37d1feace5f8c78d5dcff66681bbae
Emiliano Pedrozo <emiliano.pedrozo@numerade.coml>
2024-11-18 12:43:16 -0300

    Added first datasource

---
+++ ultronlib/datasources/quandl.py
@@ -0,0 +1,13 @@
+class Quandl(object):
+    mapper = {
+        'ticker': 'ticker',
+        'date': 'date',
+        'open': 'open',
+        'high': 'high',
+        'low': 'low',
+        'close': 'close',
+        'volume': 'volume',
+        'closeadj': None,
+        'closeunadj': None,
+        'lastupdated': 'updated'
+    }
