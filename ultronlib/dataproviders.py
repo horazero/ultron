392ca7806a145128c7a3e40e358a5178bc9c90b9
Emiliano Pedrozo <emiliano.pedrozo@numerade.coml>
2024-11-18 15:08:47 -0300

    Removed unused method

--- ultronlib/dataproviders.py
+++ ultronlib/dataproviders.py
@@ -14,7 +14,7 @@ class DataProvider(object):
             inplace=False
         )
         self.cast_columns()
-        
+
     def cast_columns(self):
         for column in  self._datasource.mapper:
             if hasattr(column, 'preprocess'):
@@ -23,8 +23,5 @@ class DataProvider(object):
 class CSVProvider(DataProvider):
 
     def __init__(self, file_path, datasource):
-        self.df = pd.read_csv(file_path)
+        self.df = pd.read_csv(file_path, dtype=str)
         super(DataProvider).__init__(datasource)
-
-    def GetDF(self):
-        return self.df
